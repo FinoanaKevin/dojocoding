79a8720161a8eb8f0ff4a9ab4adb67cf1b9ebe55
FinoanaKevin <105128439+FinoanaKevin@users.noreply.github.com>
2022-08-15 01:17:19 +0200

    Add files via upload

---
+++ delegue/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $(".bulle:nth-child(1)").animate({
+        opacity: 1
+    }, 300);
+    $(".bulle:nth-child(2)").animate({
+        opacity: 1
+    }, 500);
+    $(".bulle:nth-child(3)").animate({
+        opacity: 1
+    }, 700);
+    $(".bulle:nth-child(4)").animate({
+        opacity: 1
+    }, 900);
+});
